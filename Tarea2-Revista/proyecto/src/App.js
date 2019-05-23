942f0e2bdfc8a28c5c89f8c3db775db9ed377ab4
Juan Camilo Achury Bueno <truancamilo@hotmail.com>
2019-05-23 06:29:31 -0500

    Routing

--- Tarea2-Revista/proyecto/src/App.js
+++ Tarea2-Revista/proyecto/src/App.js
@@ -1,16 +1,155 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
-import Ballot from  './Pages/Ballot'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import Ballot from './Pages/Ballot'
+import LoginPage from './Pages/Login'
+import { Card, Col, Row } from "react-materialize/lib/";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Ballot/>
-      </header>
+    <Router>
+      <div>
+        <Header />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+        <Route path="/login" component={LoginPage} />
+      </div>
+    </Router>
+  );
+}
+function Home() {
+  return (<AuthButton/>);
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
     </div>
   );
 }
 
+function Header() {
+  return (
+      <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo right">Elecciones 2020</a>
+        <ul class="left hide-on-med-and-down">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/ballot">Votaciones</Link>
+          </li>
+          <li>
+            <PrivateRoute to="/topics">Admin</PrivateRoute>
+          </li>
+          <li>
+            <AuthButton/>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        Authentication.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+const Authentication = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+export const AuthButton = withRouter(
+  ({ history }) =>
+  Authentication.isAuthenticated ? (
+      <span>&nbsp;</span>
+    ) : (
+      <Link to="/login">Log in</Link>
+    )
+);
+
+class Login extends Component {
+  state = { redirectToReferrer: false };
+
+  login = () => {
+    Authentication.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  render() {
+    let { from } = this.props.location.state || { from: { pathname: "/" } };
+    let { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) return <Redirect to={from} />;
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
 export default App;
+
---
+++ Tarea2-Revista/proyecto/src/Pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Login extends React.Component {
+
+
+
+    render() {
+        return (<div>
+            <h3>This is the login page</h3>
+
+
+        </div>);
+    }
+}
+
+
+export default Login;
