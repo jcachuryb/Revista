38bab3c2992b3d6fab6a281ea09356c38a22007b
Juan Camilo Achury Bueno <truancamilo@hotmail.com>
2019-05-23 06:36:06 -0500

    .

---
+++ Tarea2-Revista/proyecto/src/Pages/Summary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Summary extends React.Component {
+
+
+
+    render() {
+        return (<div>
+            <h3>This is the summary page</h3>
+
+
+        </div>);
+    }
+}
+
+
+export default Summary;
